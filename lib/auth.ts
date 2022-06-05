51520f1b8dac6f4976edf14a3d7b0223a76d02f7
Dmytro <dmytro.osiychuk@gmail.com>
2022-06-05 23:54:56 +0200

    Add auth

---
+++ lib/auth.ts
@@ -0,0 +1,25 @@
+import jwt from "jsonwebtoken";
+import { NextApiRequest, NextApiResponse } from "next";
+import prisma from "./prisma";
+
+export const validateRoute = (handler) => {
+  return async (req: NextApiRequest, res: NextApiResponse) => {
+    const { ACCESS_TOKEN: token } = req.cookies;
+    if (token) {
+      let user;
+      try {
+        const { id } = jwt.verify(token, process.env.JWT_SECRET);
+        user = await prisma.user.findUnique({
+          where: { id },
+        });
+        if (!user) {
+          throw new Error("Invalid credentials");
+        }
+      } catch (err) {
+        res.status(401).json({ error: "Invalid credentials" });
+      }
+      return handler(req, res, user);
+    }
+    res.status(401).json({ error: "Invalid credentials" });
+  };
+};
