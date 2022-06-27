6db385494fb3f0a613652520dd51936dcffd73fe
Dmytro <dmytro.osiychuk@gmail.com>
2022-06-27 21:52:04 +0200

    Attempt to fix pre-render error

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,7 +1,7 @@
-import AuthForm from "../components/authForm";
+// import AuthForm from "../components/authForm";
 
-const Signup = () => {
-  <AuthForm mode="signup" />;
-};
-Signup.authPage = true;
-export default Signup;
+// const Signup = () => {
+//   <AuthForm mode="signup" />;
+// };
+// Signup.authPage = true;
+// export default Signup;
