acc36efa1ee6903de90a255dba8c70c193c913c9
Dmytro <dmytro.osiychuk@gmail.com>
2022-06-29 23:32:18 +0200

    Cleanup

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 
 const signedInPages = ["/", "/playlist", "/library"];
-
+// middleware to redirect to signin any users who do not have a valid token
 export default function middleware(req) {
   if (signedInPages.find((p) => p === req.nextUrl.pathname)) {
     const token = req.cookies.ACCESS_TOKEN;
